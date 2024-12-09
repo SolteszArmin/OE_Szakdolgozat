8599497c619d09e8523a5eb784ee5beae059c449
kazro34 <kazro34@gmail.com>
2024-12-09 18:37:05 +0100

    Fix 2

--- src/models/neural_networks/utils/Json_Loader.py
+++ src/models/neural_networks/utils/Json_Loader.py
@@ -1,37 +1,26 @@
+import torch
 from torch.utils.data import Dataset, DataLoader
 import json
 import ijson
 import os
 
 class JSONDataset(Dataset):
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.json_files = [f for f in os.listdir(file_path) if f.endswith('.json')]
-
-    def __len__(self):
-        return len(self.json_files)
+    def __init__(self, json_dir):
+        self.json_dir = json_dir
     
-    def __Jsonlen__(self):
-        return sum(1 for _ in open(self.file_path))
+    def __len__(self):
+        return sum(1 for _ in open(self.json_dir))
     
     def __getitem__(self, idx):
-        json_path = os.path.join(self.json_dir, self.json_files[idx])
-        with open(self.json_path, 'r') as f:
-            data = json.load(f)
-            return data
-        
-    def __getJSONitem__(self, idx):
-        with open(self.file_path,'r') as f:    
+        with open(self.json_dir,'r') as f:    
             for i, item in enumerate(ijson.items(f, 'item')):
                 if i == idx:
                     return self.process_item(item)
-
 
     def process_item(self, item):
         return item
 
 
-json_dir = '/path/to/json/files'
+json_dir = 'C:/Users/Kasza Róbert/Downloads/A'
 
-dataset = JSONDataset(json_dir)
-dataloader = DataLoader(dataset, batch_size=10)
+dataloader = DataLoader(JSONDataset('C:/Users/Kasza Róbert/Downloads/A/data_savt.json'), batch_size=10)
