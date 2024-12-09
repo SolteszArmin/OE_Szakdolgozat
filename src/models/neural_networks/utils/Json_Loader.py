98f63f26df7b7b0addc4b4a373cfc3d16a41aab5
kazro34 <kazro34@gmail.com>
2024-12-09 15:52:42 +0100

    fix

--- src/models/neural_networks/utils/Json_Loader.py
+++ src/models/neural_networks/utils/Json_Loader.py
@@ -1,5 +1,6 @@
 from torch.utils.data import Dataset, DataLoader
 import json
+import ijson
 import os
 
 class JSONDataset(Dataset):
@@ -10,12 +11,24 @@ class JSONDataset(Dataset):
     def __len__(self):
         return len(self.json_files)
     
+    def __Jsonlen__(self):
+        return sum(1 for _ in open(self.file_path))
+    
     def __getitem__(self, idx):
         json_path = os.path.join(self.json_dir, self.json_files[idx])
         with open(self.json_path, 'r') as f:
             data = json.load(f)
-        return data
-    
+            return data
+        
+    def __getJSONitem__(self, idx):
+        with open(self.file_path,'r') as f:    
+            for i, item in enumerate(ijson.items(f, 'item')):
+                if i == idx:
+                    return self.process_item(item)
+
+
+    def process_item(self, item):
+        return item
 
 
 json_dir = '/path/to/json/files'
