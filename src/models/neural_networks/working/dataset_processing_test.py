b7d2e5be0a98f97034ec3a4ec11f1314d76bf4ab
Kasza Róbert Gábor <117159013+kazro34@users.noreply.github.com>
2025-04-06 15:19:19 +0200

    Update dataset_processing_test.py

--- src/models/neural_networks/working/dataset_processing_test.py
+++ src/models/neural_networks/working/dataset_processing_test.py
@@ -155,7 +155,7 @@ class DatasetProcessing():
                 v = dict(v)
                 ego = v.pop("vehicle EGO",False)
                 ego_rotation=ego["Rotation"]
-                ego_position=ego["Position"]
+                ego_position=ego["relative_Position"]
                 ego_velocity=ego["Velocity"]
                 ego_trafic_l_state=self.mapper_d["Traffic_Light_State"][ego["Traffic_Light_State"]]
 
